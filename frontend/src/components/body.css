ae6fae7df26ef6a06c737dabe3da4e30a541653f
Malin <malinlundhe@hotmail.com>
2024-03-05 15:10:23 +0100

    started with code in body.jsx

---
+++ frontend/src/components/body.css
@@ -0,0 +1,18 @@
+import React from "react";
+import './body.css';
+import { Header } from './header/Header';
+import { Skills } from './skills/Skills';
+import { Carousell } from './carousell/Carousell';
+import { Footer } from './footer/Footer';
+
+export const Body = () => {
+    return (
+        <div classname="big-wrapper">
+         <Header />
+         <Skills />
+         <Carousell />
+         <Footer />
+    );
+};
+
+export default Body; 
