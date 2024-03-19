71c2037c106239be5fd744a99444797bb8732e2b
Malin <malinlundhe@hotmail.com>
2024-03-19 14:31:43 +0100

    added intro files

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -4,14 +4,17 @@ import { Header } from './header/Header';
 import { Skills } from './skills/Skills';
 import { Carousell } from './carousell/Carousell';
 import { Footer } from './footer/Footer';
+import { Intro } from './intro/Intro';
 
 export const Body = () => {
     return (
         <div classname="big-wrapper">
          <Header />
+         <Intro />
          <Skills />
          <Carousell />
          <Footer />
+        </div>
     );
 };
 
