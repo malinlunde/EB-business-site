612131aa33a31d346f9aef72201a04fdaadb5225
Malin <malinlundhe@hotmail.com>
2024-03-19 17:34:13 +0100

    added icons and images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './footer.css';
+import linkedin from '../icons/linkedin.svg';
+import instagram from '../icons/instagram.svg';
 
 export const Footer = () => {
+    const linkedinLink = 'https://www.linkedin.com/in/erica-b%C3%B6rjesson-a7166345/';
+    const instagramLink = 'https://www.instagram.com/livetsomerica';
+
     return (
         <footer className="footer">
             <div className="footer-content">
-                <h1>Kontakta mig</h1>
+                <h1>Kontakt</h1>
                 <ul className="contact-list">
-                    <li>Telefon: 123-456-789</li>
-                    <li><a href="https://www.instagram.com/livetsomerica">Instagram</a></li>
-                    <li><a href="https://www.linkedin.com/in/your_linkedin">LinkedIn</a></li>
-                    <li><a href="https://www.facebook.com/your_facebook">Facebook</a></li>
+                    <li>Telefon: +46 (0) 70 218 78 34</li>
+                    <li><a href={instagramLink} target="_blank" rel="noopener noreffer">
+                        <img src={instagram} alt='Instagram' className='instagram' />
+                        </a></li>
+                    <li><a href={linkedinLink} target="_blank" rel="noopener noreffer">
+                        <img src={linkedin} alt='linkedin' className='linkedin' />
+                        </a></li>
                     <li>Email: your_email@example.com</li>
                 </ul>
             </div>
