7f94c8f406524760cc5cf1c508b9e879c7c57b50
Malin <malinlundhe@hotmail.com>
2024-03-20 19:30:50 +0100

    adding projectpage and code for carousell

--- src/components/carousell/Carousell.jsx
+++ src/components/carousell/Carousell.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './carousell.css';
+import { Link } from 'react-router-dom';
+
+const projects = [
+    { id: 1, title: 'Project 1', description: 'Description for Project 1', imageUrl: '/project1.jpg' }, 
+    { id: 1, title: 'Project 1', description: 'Description for Project 1', imageUrl: '/project1.jpg' }, 
+    { id: 1, title: 'Project 1', description: 'Description for Project 1', imageUrl: '/project1.jpg' }, 
+    { id: 1, title: 'Project 1', description: 'Description for Project 1', imageUrl: '/project1.jpg' }, 
+    { id: 1, title: 'Project 1', description: 'Description for Project 1', imageUrl: '/project1.jpg' }, 
+];
 
 export const Carousell = () => {
     return (
         <section className="carousell">
             <div className="carousell-content">
-                {/* Carousel content will go here */}
+                {projects.map(project => (
+                    <div key={project.id} className="project-item">
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                        <img src={project.imageUrl} alt={project.title} />
+                        <Link to={`/project/${project.id}`}>See More</Link>
+                    </div>
+                ))}
             </div>
         </section>
     );
