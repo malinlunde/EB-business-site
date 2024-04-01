a72bd7442c0aa81132656c7a15629f72fe0cd44e
Malin <malinlundhe@hotmail.com>
2024-04-01 20:33:25 +0200

    added styling to intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './intro.css';
+import profileImage from '../images/profilbild.jpeg';
 
 export const Intro = () => {
     return (
         <section className="intro">
             <div className="intro-content">
+                <img src={profileImage} alt="Profile" className="profile-image" />
                 <h1>Erica Börjesson</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl vel nisi finibus fringilla. Nulla facilisi. Nam eu nulla nisi.</p>
-                {/* You can add an image here once you have it */}
+                
          </div>
         </section> 
     );
