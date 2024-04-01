426a69a4ea6c64852898a71200e0826bc9384617
Malin <malinlundhe@hotmail.com>
2024-04-01 17:06:13 +0200

    added images

--- src/components/carousell/Carousell.jsx
+++ src/components/carousell/Carousell.jsx
@@ -3,62 +3,97 @@ import './carousell.css';
 import { Link } from 'react-router-dom';
 import leftArrow from '../images/leftarrow.png';
 import rightArrow from '../images/rightarrow.png';
+import BruksholmenHeader from '../images/Bruksholmen.svg';
+import bruksholmen from '../images/bruksholmen.jpeg';
+import bruksholmen1 from '../images/bruksholmen1.jpg';
+import bruksholmen2 from '../images/bruksholmen2.jpg';
+import bruksholmen3 from '../images/bruksholmen3.jpg';
+import bruksholmen4 from '../images/bruksholmen4.jpg';
+import bruksholmen5 from '../images/bruksholmen5.jpg';
+import bruksholmen6 from '../images/bruksholmen6.jpg';
+import bruksholmen7 from '../images/bruksholmen7.jpg';
+import bruksholmen8 from '../images/bruksholmen8.jpg';
+import bruksholmen9 from '../images/bruksholmen9.jpg';
+import weddingheader from '../images/weddingheader.svg';
+import wedding1 from '../images/wedding1.jpeg';
+import wedding2 from '../images/wedding2.jpeg';
+import interior from '../images/Interior.svg';
+import nyttkontor1 from '../images/nyttkontor1.jpeg';
+import nyttkontor2 from '../images/nyttkontor2.jpeg';
+import nyttkontor3 from '../images/nyttkontor3.jpeg';
+import singerheader from '../images/Singer .svg';
+import skidorheader from '../images/Skidor.svg';
+import skidor1 from '../images/skidor.jpeg';
+import skidor2 from '../images/skidor2.jpg';
+import skidor3 from '../images/skidor3.jpg';
+import singer1 from '../images/singer1.jpg';
+
+
 
 const projects = [
     { 
         id: 1, 
-        title: 'Project 1', 
-        description: 'Description for Project 1', 
+        title: 'Bruksholmen Forssjö', 
+        description: 'Från bryggan vid Forssjöbadet såg jag de tomma vackra gamla brukslokalerna. Dem renoverades och vi kunde flytta in...', 
         images: [
-            '/project1-image1.jpg',
-            '/project1-image2.jpg',
-            '/project1-image3.jpg'
+            bruksholmen,
+            bruksholmen1,
+            bruksholmen2,
+            bruksholmen3,
+            bruksholmen4,
+            bruksholmen5,
+            bruksholmen6,
+            bruksholmen7,
+            bruksholmen8,
+            bruksholmen9,
         ],
-        additionalText: 'Additional text for Project 1'
+        headerImage: BruksholmenHeader,
+        additionalText: 'Från bryggan vid Forssjöbadet såg jag de tomma vackra gamla brukslokalerna. Dem renoverades och vi kunde flytta in. Vi skapade en restaurang med dagens lunch, a la carte meny, napolitansk pizza, goda drycker och smarriga bakverk. En inredningsbutik/klädbutik med utvalda favoriter. En 24-food butik där vi sålde våra egna matlådor och tog vara på det som annars hade blivit svinn. Eventloge där vi anordnade konserter med kända svenska artister tack vare samarbete med Henrik Olsson. Vi anordnade även trädgårdsfest med liveband, afterski-fester, stand-up samt bröllop och privata fester. Under våren blev logen istället en blomsterloge med trädgårdshandel. Mitt mål var att skapa ”Ett café med Napolitansk pizza fullt av snygg inredning som gick att köpa samtidigt som ett lokalt eller stort band/artist stod på en liten scen och gav oss en magisk upplevelse. Viktigt är att det blir en plats att träffa nya bekantskaper och en familjär känsla med stammisar.”'
     }, 
     { 
         id: 2, 
-        title: 'Project 2', 
-        description: 'Description for Project 2', 
+        title: 'Event- & bröllopskoordinator', 
+        description: 'Planerar ni er stora dag eller kanske en större fest?', 
         images: [
-            '/project2-image1.jpg',
-            '/project2-image2.jpg',
-            '/project2-image3.jpg'
+            wedding1,
+            wedding2,
         ],
-        additionalText: 'Additional text for Project 2'
+        headerImage: weddingheader,
+        additionalText: 'Planerar ni er stora dag eller kanske en större fest men saknar en samordnare eller lite hjälp i starten. Boka ett möte med mig så gör vi en första plan. Ibland stannar vi där eller så önskar ni hjälp ända till städningen efter festen, här bestämmer ni förutsättningarna. Är det dags för er förening att skapa en årsdag eller kanske önskar ni ha en artist på er scen, event av alla dess slag är min absoluta styrka.'
     }, 
     { 
         id: 3, 
-        title: 'Project 3', 
-        description: 'Description for Project 3', 
+        title: 'Projektledare & Inredning', 
+        description: 'Nätverk och mötet med nya människor har alltid drivit mig framåt...', 
         images: [
-            '/project3-image1.jpg',
-            '/project3-image2.jpg',
-            '/project3-image3.jpg'
+            nyttkontor1,
+            nyttkontor2,
+            nyttkontor3,
         ],
-        additionalText: 'Additional text for Project 3'
+        headerImage: interior,
+        additionalText: 'Nätverk och mötet med nya människor har alltid drivit mig framåt. Projektledarrollen ger mig möjlighet att tillsammans med fantastiska personer anordna meditativa träffar, event, bröllop, styling mm. Se mer om vad jag kan erbjuda och vad jag gjort här på sidan.'
     }, 
     { 
         id: 4, 
-        title: 'Project 4', 
-        description: 'Description for Project 4', 
+        title: 'Sångerska', 
+        description: 'Att få förtroendet att sjunga någons favoritsång på en högtidsdag är en ynnest.', 
         images: [
-            '/project4-image1.jpg',
-            '/project4-image2.jpg',
-            '/project4-image3.jpg'
+            singer1,
         ],
-        additionalText: 'Additional text for Project 4'
+        headerImage: singerheader,
+        additionalText: 'Att få förtroendet att sjunga någons favoritsång på en högtidsdag är en ynnest. Anlita mig gärna som sångerska till ditt bröllop, dop eller vid begravning. Självklart skickas ljudfiler innan ni bestämmer er. Jag har även kontakt med musiker för komp.'
     }, 
     { 
         id: 5, 
-        title: 'Project 5', 
-        description: 'Description for Project 5', 
+        title: 'Skidlärare', 
+        description: 'Nybörjare eller vill utbeckla din teknik?',
         images: [
-            '/project5-image1.jpg',
-            '/project5-image2.jpg',
-            '/project5-image3.jpg'
+            skidor1,
+            skidor2,
+            skidor3,
         ],
-        additionalText: 'Additional text for Project 5'
+        headerImage: skidorheader,
+        additionalText: 'Efter slalomutbildningar och arbete som skidlärare i Idre lär jag mer än gärna ut mina kunskaper. Är du nybörjare eller önskar utveckla din teknik, kontakta mig så skapar vi minnen tillsammans.',
     },  
 ];
 
@@ -85,7 +120,7 @@ export const Carousell = () => {
                 <div className="project-item">
                     <h2>{projects[currentProjectIndex].title}</h2>
                     <p>{projects[currentProjectIndex].description}</p>
-                    <img src={projects[currentProjectIndex].images[0]} alt={projects[currentProjectIndex].title} />
+                    <img src={projects[currentProjectIndex].image} alt={projects[currentProjectIndex].title} />
                     <Link className="see-more-link" to={`/project/${projects[currentProjectIndex].id}`}>Se mer</Link> 
                 </div>
             </div>
--- src/components/projectpage/ProjectPage.jsx
+++ src/components/projectpage/ProjectPage.jsx
@@ -8,19 +8,22 @@ export const ProjectPage = () => {
     const project = projects.find(project => project.id === parseInt(id));
 
     return (
-    <div>
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
-        {/* Display additional images */}
-        {project.images.map((image, index) => (
-            <img key={index} src={image} alt={`Image ${index + 1}`} />
-        ))}
-        {/* Display additional text */}
-        <p>{project.additionalText}</p>
-        {/* Additional details about the project */}
-        
-    </div>
-    );
-};
-
-export default ProjectPage;
+        <div>
+            <img src={project.headerImage} alt={project.title} />
+            <h1>{project.title}</h1>
+            <p>{project.description}</p>
+            {/* Display additional images */}
+            <div className="additional-images">
+                {project.images.map((image, index) => (
+                    <img key={index} src={image} alt={`Image ${index + 1}`} />
+                ))}
+            </div>
+            {/* Display additional text */}
+            <p>{project.additionalText}</p>
+            {/* Additional details about the project */}
+            
+        </div>
+        );
+    };
+    
+    export default ProjectPage;
