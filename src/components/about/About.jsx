9831a070c33b9a2508215c935e174403c2ae111a
Malin <malinlundhe@hotmail.com>
2024-04-03 13:12:45 +0200

    added content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,20 +6,12 @@ export const About = () => {
         <section className="about">
             <div className="about-content">
                 <h1>Hej & Välkommen till min värld!</h1>
-                <p>Jag är Erica Börjesson, entreprenör och kreativ kraft som bor i den idylliska byn Forssjö utanför Katrineholm tillsammans med min man och våra två underbara barn. Att skapa och driva företag är min passion, och jag är stolt över att ha förvandlat gamla brukslokaler till levande och pulserande platser som berikar vår lokala gemenskap.
-
-Mitt företagsverk har varit en mångfacetterad resa där jag har förenat mina intressen för mat, inredning, musik och natur. Från att ha drivit restaurang med napolitansk pizza till att arrangera konserter med kända artister, har jag strävat efter att skapa platser och evenemang där människor kan mötas, njuta och skapa minnen tillsammans.
-
-Projektledning är en av mina starka sidor, och jag älskar att samarbeta med fantastiska människor för att planera och genomföra allt från bröllop och privata fester till företagsevent och konserter. Att se idéer ta form och bli till verkliga upplevelser är en av de mest givande delarna av mitt arbete.
-
-Men mitt engagemang sträcker sig bortom affärsvärlden. Som sångerska är det en ära för mig att få delta i människors högtidsstunder, vare sig det är vid bröllop, dop eller begravningar. Musiken har alltid varit en central del av mitt liv, och jag strävar efter att förmedla känslor och skapa minnesvärda ögonblick genom sången.
-
-När jag inte arbetar eller uppträder på scenen, hittar du mig antingen på skidbackarna eller i naturen. Jag har en djup förkärlek för skidåkning och att utforska den vackra omgivningen runt Forssjö.
-
-Att bli utnämnd till Katrineholms ambassadör är en ära och ett bevis på mitt engagemang för vår lokala gemenskap. Jag brinner för att skapa positiva förändringar och bidra till att göra vår by och dess omgivningar till en ännu bättre plats att leva och verka i.
-
-Jag ser fram emot att möta dig och utforska möjligheterna att skapa magiska stunder tillsammans. Tveka inte att kontakta mig för att diskutera idéer eller samarbeten. Låt oss skapa något extraordinärt!</p>
-                {/* You can add more content about yourself here */}
+                <p>Jag är Erica Börjesson, entreprenör och kreativ kraft som bor i den idylliska byn Forssjö utanför Katrineholm tillsammans med min man och våra två underbara barn. Att skapa och driva företag är min passion, och jag är stolt över att ha förvandlat gamla brukslokaler till levande och pulserande platser som berikar vår lokala gemenskap. </p>
+                <p>Mitt företagsverk har varit en mångfacetterad resa där jag har förenat mina intressen för mat, inredning, musik och natur. Från att ha drivit restaurang med napolitansk pizza till att arrangera konserter med kända artister, har jag strävat efter att skapa platser och evenemang där människor kan mötas, njuta och skapa minnen tillsammans.</p>
+                <p>Projektledning är en av mina starka sidor, och jag älskar att samarbeta med fantastiska människor för att planera och genomföra allt från bröllop och privata fester till företagsevent och konserter. Att se idéer ta form och bli till verkliga upplevelser är en av de mest givande delarna av mitt arbete.</p>
+                <p>Men mitt engagemang sträcker sig bortom affärsvärlden. Som sångerska är det en ära för mig att få delta i människors högtidsstunder, vare sig det är vid bröllop, dop eller begravningar. Musiken har alltid varit en central del av mitt liv, och jag strävar efter att förmedla känslor och skapa minnesvärda ögonblick genom sången.</p>
+                <p>När jag inte arbetar eller uppträder på scenen, hittar du mig antingen på skidbackarna eller i naturen. Jag har en djup förkärlek för skidåkning och att utforska den vackra omgivningen runt Forssjö.</p>
+                <p>Att bli utnämnd till Katrineholms ambassadör är en ära och ett bevis på mitt engagemang för vår lokala gemenskap. Jag brinner för att skapa positiva förändringar och bidra till att göra vår by och dess omgivningar till en ännu bättre plats att leva och verka i. Jag ser fram emot att möta dig och utforska möjligheterna att skapa magiska stunder tillsammans. Tveka inte att kontakta mig för att diskutera idéer eller samarbeten. Låt oss skapa något extraordinärt!</p>
             </div>
         </section>
     );
