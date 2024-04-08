6cf73b519f515168b12c7777715dc19ecdcab296
Malin <malinlundhe@hotmail.com>
2024-04-08 21:19:59 +0200

    styling changes

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './skills.css';
 import bruksholmen9 from '../images/bruksholmen9.png';
-import projectman from '../images/projectman.svg';
+import bruksholmen5 from '../images/bruksholmen5.jpg';
 
 export const Skills = () => {
     return (
     <div className='skills-wrapper'>
         <section className="skills">
             <div className="skills-content">
-                <img src={projectman} alt="Image" className='projectman-image' />
+                <img src={bruksholmen5} alt="Image" className="bruksholmen-image" />
                 <img src={bruksholmen9} alt="Image" className="bruksholmen-image" />
-                
             </div>
         </section>
     </div>
