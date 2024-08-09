2cee534608641c5c944b9091bf20e20c96098296
Malin <malinlundhe@hotmail.com>
2024-08-09 14:12:00 +0200

    added contact and rewrite footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './contact.css'; // Create this CSS file for styling the contact form
 import { fetchPageContent } from '../../services/wordpressService';
+import linkedin from '../icons/linkedin.svg';
+import instagram from '../icons/instagram.svg';
 
 export const Contact = () => {
     const [formContent, setFormContent] = useState('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const linkedinLink = 'https://www.linkedin.com/in/erica-b%C3%B6rjesson-a7166345/';
+    const instagramLink = 'https://www.instagram.com/livetsomerica';
+    const emailAddress = 'kontakt@ericaborjesson.se';
 
     useEffect(() => {
         const getContent = async () => {
@@ -36,6 +41,18 @@ export const Contact = () => {
             <div className="contact-content">
                 <h2>Kontakta mig</h2>
                 <div dangerouslySetInnerHTML={{ __html: formContent }} />
+                <div className="contact-info">
+                    <p>Telefon: +46 (0) 12 345 67</p>
+                    <p>E-post: <a href={`mailto:${emailAddress}`}>{emailAddress}</a></p>
+                </div>
+                <div className="social-links">
+                    <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                        <img src={instagram} alt='Instagram' className='instagram' />
+                    </a>
+                    <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
+                        <img src={linkedin} alt='linkedin' className='linkedin' />
+                    </a>
+                </div>
             </div>
         </section>
     );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,32 +1,13 @@
-import React from 'react';
 import './footer.css';
-import linkedin from '../icons/linkedin.svg';
-import instagram from '../icons/instagram.svg';
-
 export const Footer = () => {
-    const linkedinLink = 'https://www.linkedin.com/in/erica-b%C3%B6rjesson-a7166345/';
-    const instagramLink = 'https://www.instagram.com/livetsomerica';
-    const emailAddress = 'malinlundhe@hotmail.com';
-
+  
     return (
-        <footer className="footer">
-        <div className="footer-content">
-            <h1>Kontakt</h1>
-            <div className="contact-info">
-                <p>Telefon: +46 (0) 12 345 67</p>
-                <p>E-post: <a href={`mailto:${emailAddress}`}>{emailAddress}</a></p>
+        <section className='footer'>
+            <div className='CreatedBy'>
+                (C) 2024 Skapad av:  <a href="https://thetechadminsphere.com/"> thetechadminsphere</a>
             </div>
-            <div className="social-links">
-                <a href={instagramLink} target="_blank" rel="noopener noreferrer">
-                    <img src={instagram} alt='Instagram' className='instagram' />
-                </a>
-                <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
-                    <img src={linkedin} alt='linkedin' className='linkedin' />
-                </a>
-            </div>
-        </div>
-    </footer>
-);
-};
-
-export default Footer;
+        </section>
+    );
+  };
+  
+  export default Footer;
